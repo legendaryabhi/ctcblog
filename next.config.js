2fd4eb6e83ca3ade77923ac5b6b035e35df93389
Abhinav Sharma <85983956+legendaryabhi@users.noreply.github.com>
2024-03-12 23:32:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const {withContentlayer} = require("next-contentlayer")
 
 const nextConfig = {
     output: "export",
+    images: { unoptimized: true } ,
     compiler:{
         removeConsole: true,
     }
